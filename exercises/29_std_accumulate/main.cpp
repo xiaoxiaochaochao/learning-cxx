fec5baec04f33e6d8607bb76529bdce7db273ae8
xiaochao16 <437951205@qq.com>
2024-07-31 11:38:17 +0800

    28 29

--- exercises/29_std_accumulate/main.cpp
+++ exercises/29_std_accumulate/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include "../exercise.h"
 #include <numeric>
-
-// READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
+int acc(int a, int b) {
+    return a * b;
+}
 
 int main(int argc, char **argv) {
     using DataType = float;
@@ -11,7 +12,7 @@ int main(int argc, char **argv) {
     //       - 形状为 shape；
     //       - 连续存储；
     //       的张量占用的字节数
-    // int size =
+    int size = std::accumulate(shape, shape + 4, 1, acc);
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
